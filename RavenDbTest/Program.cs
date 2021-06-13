feea5fc2fbb2e5e2d48bb998331bdbfb5cc186bd
nitin <nitin@DESKTOP-3LUEEJU>
2021-06-13 22:15:12 +0530

    Add project files.

---
+++ RavenDbTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using System;
+
+namespace RavenDbTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDocumentStore store = new DocumentStore { Urls = new[]{ "http://localhost:8080" } , Database = "Persons"};
+            store.Initialize();
+
+            var person = new Person { Name = "Nitin" };
+            IDocumentSession session = store.OpenSession();
+            session.Store(person);
+            session.SaveChanges();
+
+            Person loadedPerson = session.Load<Person>(person.Id.ToString());
+
+            Console.WriteLine(loadedPerson.Name);
+        }
+    }
+
+    public record Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
